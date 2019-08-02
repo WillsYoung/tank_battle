7de742153f4c33063e6c606e2118b4b16ce41740
WillsYoung <1113792848@qq.com>
2019-08-02 21:56:38 +0800

    重构

---
+++ manage.py
@@ -0,0 +1,27 @@
+from time import sleep
+
+import pygame
+
+from config.base_config import SCREEN_SIZE
+
+
+
+
+def main():
+
+
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption('坦克大战')
+
+    while True:
+        print("hello")
+        pygame.
+        sleep(2)
+        break
+
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
